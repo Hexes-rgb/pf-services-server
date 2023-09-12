97899c8f268c4dc9bd22da3634b95aa9d2c5baae
artem.kuskin <artem.kuskin@progforce.com>
2023-09-12 09:26:49 +0300

    fix

--- src/wish-card/wish-card.controller.ts
+++ src/wish-card/wish-card.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Param, Post, Req, Res } from '@nestjs/common';
+import { Body, Controller, Delete, Param, Post, Put, Req, Res } from '@nestjs/common';
 import { WishCardService } from './wish-card.service';
 import { CreateWishCardDto } from './dto/create-wish-card.dto';
 import { Request, Response } from 'express';
@@ -14,14 +14,14 @@ export class WishCardController {
         return this.wishCardService.createWishCard(wishCardDto, token)
     }
 
-    @Post('change/:id')
+    @Put('change/:id')
     async changeWishCard(@Param() param, @Body() wishCardDto: CreateWishCardDto, @Req() req: Request) {
         const authorization = req.headers['authorization']
         const token = authorization?.split(' ')[1]
         return await this.wishCardService.changeWishCard(param.id, token, wishCardDto)
     }
 
-    @Post('delete/:id')
+    @Delete('delete/:id')
     async deleteWishCard(@Param() param) {
         return await this.wishCardService.deleteWishCard(param.id)
     }
