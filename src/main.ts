ee114ab0419026e2ea56fca4e45bafdd406fbedf
Pavel Topilin <p.topilin@inbox.ru>
2023-09-25 14:32:46 +0300

    Add CORS

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as cookieParser from 'cookie-parser';
+import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const corsOptions: CorsOptions = {
+    origin: ['http://localhost:3001'],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  };
+  app.enableCors(corsOptions);
   app.use(cookieParser());
   await app.listen(3000);
 }
